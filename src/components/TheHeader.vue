01a05459ccc138dc27eecd0e4767ce93d9d75b6b
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-08-05 11:35:05 +0500

    Initial commit and add teachers page

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,190 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="bg-[#07294d] text-[#c6c6c6] font-semibold py-4">
+      <div class="container mx-auto w-full max-w-7xl px-5 flex items-center justify-between">
+        <div class="flex">
+          <div class="mr-5 flex items-center gap-2">
+            <svg
+              class="w-4"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
+              />
+            </svg>
+            <a href="#">nammqi_info@edu.uz</a>
+          </div>
+          <div class="mr-5 flex items-center gap-2">
+            <svg
+              class="w-4"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"
+              />
+            </svg>
+            <a href="tel:+998692341430">+998 (69) 234-14-30</a>
+          </div>
+        </div>
+        <div class="flex">
+          <span class="block mr-2.5">Ijtimoiy tarmoqlar: </span>
+          <ul class="flex items-center gap-2 mr-6">
+            <li class="px-[5px]">
+              <a href="#">
+                <svg
+                  class="w-2"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path
+                    d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"
+                  />
+                </svg>
+              </a>
+            </li>
+            <li class="px-[5px]">
+              <a href="#">
+                <svg
+                  class="w-4"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    d="M459.4 151.7c.3 4.5 .3 9.1 .3 13.6 0 138.7-105.6 298.6-298.6 298.6-59.5 0-114.7-17.2-161.1-47.1 8.4 1 16.6 1.3 25.3 1.3 49.1 0 94.2-16.6 130.3-44.8-46.1-1-84.8-31.2-98.1-72.8 6.5 1 13 1.6 19.8 1.6 9.4 0 18.8-1.3 27.6-3.6-48.1-9.7-84.1-52-84.1-103v-1.3c14 7.8 30.2 12.7 47.4 13.3-28.3-18.8-46.8-51-46.8-87.4 0-19.5 5.2-37.4 14.3-53 51.7 63.7 129.3 105.3 216.4 109.8-1.6-7.8-2.6-15.9-2.6-24 0-57.8 46.8-104.9 104.9-104.9 30.2 0 57.5 12.7 76.7 33.1 23.7-4.5 46.5-13.3 66.6-25.3-7.8 24.4-24.4 44.8-46.1 57.8 21.1-2.3 41.6-8.1 60.4-16.2-14.3 20.8-32.2 39.3-52.6 54.3z"
+                  />
+                </svg>
+              </a>
+            </li>
+            <li class="px-[5px]">
+              <a href="#">
+                <svg
+                  class="w-4"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 496 512"
+                >
+                  <path
+                    d="M248 8C111 8 0 119 0 256S111 504 248 504 496 393 496 256 385 8 248 8zM363 176.7c-3.7 39.2-19.9 134.4-28.1 178.3-3.5 18.6-10.3 24.8-16.9 25.4-14.4 1.3-25.3-9.5-39.3-18.7-21.8-14.3-34.2-23.2-55.3-37.2-24.5-16.1-8.6-25 5.3-39.5 3.7-3.8 67.1-61.5 68.3-66.7 .2-.7 .3-3.1-1.2-4.4s-3.6-.8-5.1-.5q-3.3 .7-104.6 69.1-14.8 10.2-26.9 9.9c-8.9-.2-25.9-5-38.6-9.1-15.5-5-27.9-7.7-26.8-16.3q.8-6.7 18.5-13.7 108.4-47.2 144.6-62.3c68.9-28.6 83.2-33.6 92.5-33.8 2.1 0 6.6 .5 9.6 2.9a10.5 10.5 0 0 1 3.5 6.7A43.8 43.8 0 0 1 363 176.7z"
+                  />
+                </svg>
+              </a>
+            </li>
+            <li class="px-[5px]">
+              <a href="#">
+                <svg
+                  class="w-4"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path
+                    d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+                  />
+                </svg>
+              </a>
+            </li>
+          </ul>
+          <div class="flex gap-2 text-[#ffc600]">
+            <a href="#">Login</a>
+            <span> / </span>
+            <a href="#">Register</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-white font-semibold py-5">
+      <div class="container mx-auto w-full max-w-7xl px-5 flex items-center justify-between">
+        <a href="#">
+          <img src="@/assets/images/logo-uz.png" width="250" alt="Logo" />
+        </a>
+        <ul class="flex items-center">
+          <li>
+            <a class="px-2.5 hover:text-[#07294d] transition-all" href="#">Tuzilma</a>
+          </li>
+          <li>
+            <a class="px-2.5 hover:text-[#07294d] transition-all" href="#">Faoliyat</a>
+          </li>
+          <li>
+            <a class="px-2.5 hover:text-[#07294d] transition-all" href="#">Ta'lim</a>
+          </li>
+          <li>
+            <a class="px-2.5 hover:text-[#07294d] transition-all" href="#">Abiturient</a>
+          </li>
+          <li>
+            <a class="px-2.5 hover:text-[#07294d] transition-all" href="#">Yangiliklar</a>
+          </li>
+          <li>
+            <a class="px-2.5 hover:text-[#07294d] transition-all" href="#">Hujjatlar</a>
+          </li>
+          <li>
+            <a class="px-2.5 hover:text-[#07294d] transition-all" href="#">NamMQI Green</a>
+          </li>
+          <li>
+            <a class="px-2.5 hover:text-[#07294d] transition-all" href="#">QABUL-2024</a>
+          </li>
+          <li>
+            <a class="px-2.5 hover:text-[#07294d] transition-all" href="#">Bog'lanish</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="bg-[#07294d] text-[#c6c6c6] font-semibold py-4 flex">
+      <div class="container mx-auto w-full max-w-7xl px-5 flex items-center justify-between">
+        <div class="flex justify-center w-full">
+          <div class="w-[40%] relative">
+            <input class="search-input outline-none" type="text" placeholder="Search teacher" />
+            <svg
+              class="absolute w-5 left-3 top-1/2 -translate-y-1/2"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
+              />
+            </svg>
+          </div>
+          <button class="search-btn">Search</button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.search-input {
+  width: 100%;
+  background: url(../images/search.png) no-repeat left center #ffffff;
+  background-size: 17px;
+  background-position-x: 12px;
+  color: #464646;
+  font-weight: 500;
+  text-transform: capitalize;
+  border: 1px solid #ffffff;
+  padding: 8px 45px;
+  height: 100%;
+  border-top-left-radius: 2px;
+  border-bottom-left-radius: 2px;
+  font-size: 14px;
+}
+
+.search-btn {
+  border: 0px;
+  color: #fff;
+  font-weight: 600;
+  text-transform: uppercase;
+  padding: 13px;
+  border: 1px solid #e25f30;
+  border-top-right-radius: 2px;
+  border-bottom-right-radius: 2px;
+  background: linear-gradient(to top, #de592a, #f36b3b);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,198 @@
+<script setup lang="ts">
+import TheHeader from '@/components/TheHeader.vue'
+</script>
+
+<template>
+  <the-header />
+
+  <main>
+    <div class="hero"></div>
+    <div class="container mx-auto w-full max-w-7xl px-5 py-[100px]">
+      <div class="text-center pb-10">
+        <span class="font-medium uppercase mb-4 text-base text-[#808080]">Instructors</span>
+        <h2 class="mb-1 font-bold text-4xl">Course Instructors</h2>
+      </div>
+      <ul class="flex flex-wrap">
+        <li v-for="i in 10" :key="i" class="w-1/3 p-4">
+          <div class="card-wrapper">
+            <div class="card-inner">
+              <div class="thumbnail-wrapper">
+                <div class="thumbnail">
+                  <a href="#">
+                    <img class="rounded-[10px]" src="../assets/images/team-06.webp" alt="" />
+                  </a>
+                </div>
+                <ul class="thumbnail-social flex gap-4">
+                  <li class="flex items-center justify-center">
+                    <a
+                      class="flex border-2 text-white hover:bg-white hover:text-[#07294d] transition-all duration-300 w-full h-full p-3 rounded-full"
+                      href="#"
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 448 512"
+                      >
+                        <path
+                          d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                  <li class="flex items-center justify-center">
+                    <a
+                      class="flex border-2 text-white hover:bg-white hover:text-[#07294d] transition-all duration-300 w-full h-full p-3 rounded-full"
+                      href="#"
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                      >
+                        <path
+                          d="M459.4 151.7c.3 4.5 .3 9.1 .3 13.6 0 138.7-105.6 298.6-298.6 298.6-59.5 0-114.7-17.2-161.1-47.1 8.4 1 16.6 1.3 25.3 1.3 49.1 0 94.2-16.6 130.3-44.8-46.1-1-84.8-31.2-98.1-72.8 6.5 1 13 1.6 19.8 1.6 9.4 0 18.8-1.3 27.6-3.6-48.1-9.7-84.1-52-84.1-103v-1.3c14 7.8 30.2 12.7 47.4 13.3-28.3-18.8-46.8-51-46.8-87.4 0-19.5 5.2-37.4 14.3-53 51.7 63.7 129.3 105.3 216.4 109.8-1.6-7.8-2.6-15.9-2.6-24 0-57.8 46.8-104.9 104.9-104.9 30.2 0 57.5 12.7 76.7 33.1 23.7-4.5 46.5-13.3 66.6-25.3-7.8 24.4-24.4 44.8-46.1 57.8 21.1-2.3 41.6-8.1 60.4-16.2-14.3 20.8-32.2 39.3-52.6 54.3z"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                  <li class="flex items-center justify-center">
+                    <a
+                      class="flex border-2 text-white hover:bg-white hover:text-[#07294d] transition-all duration-300 w-full h-full p-3 rounded-full"
+                      href="#"
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 320 512"
+                      >
+                        <path
+                          d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="content text-center mt-4">
+                <h5 class="title mb-1 font-semibold text-lg">
+                  <a href="#">Jane Seymour</a>
+                </h5>
+                <span class="designation mb-4 text-[#808080]">Teacher</span>
+                <p class="mb-4 text-[#808080] text-base">
+                  Consectetur adipisicing elit, sed do eius mod tempor incididunt
+                </p>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.hero {
+  background: url(@/assets/images/pro-bg.jpg) no-repeat center center;
+  width: 100%;
+  height: 250px;
+  display: block;
+  background-size: cover;
+}
+
+.card-wrapper {
+  padding: 0 30px 36px;
+  position: relative;
+  z-index: 1;
+}
+
+.card-wrapper::after {
+  content: '';
+  height: 75%;
+  width: 100%;
+  background-color: white;
+  border-radius: 10px;
+  /* box-shadow: 0 10px 50px 0 rgba(26, 46, 85, 0.1); */
+  box-shadow: 0 10px 50px 0 rgba(7, 41, 77, 0.1);
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+}
+
+.thumbnail a {
+  border-radius: 10px;
+  position: relative;
+  display: block;
+  transition: 0.3s;
+}
+
+.thumbnail a:after {
+  content: '';
+  height: 100%;
+  width: 100%;
+  /* background-color: rgba(26, 182, 157, 0.6); */
+  background-color: rgba(7, 41, 77, 0.5);
+  border-radius: 10px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  visibility: visible;
+  opacity: 0;
+  -webkit-transition: 0.3s;
+  transition: 0.3s;
+}
+
+.card-wrapper:hover .thumbnail a:after {
+  visibility: visible;
+  opacity: 1;
+}
+
+.card-wrapper:hover .thumbnail-social li:nth-child(1n) {
+  transition-delay: 0.2s;
+}
+.card-wrapper:hover .thumbnail-social li:nth-child(2n) {
+  transition-delay: 0.3s;
+}
+.card-wrapper:hover .thumbnail-social li:nth-child(3n) {
+  transition-delay: 0.4s;
+}
+
+.card-wrapper:hover .thumbnail-social li {
+  -webkit-transform: translateX(0);
+  -ms-transform: translateX(0);
+  transform: translateX(0);
+  visibility: visible;
+  opacity: 1;
+}
+
+.thumbnail-social {
+  transition: 0.3s;
+  display: flex;
+  justify-content: center;
+  margin: -5px;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  right: 0;
+  -webkit-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+
+.thumbnail-social li {
+  -webkit-transform: translateX(10px);
+  -ms-transform: translateX(10px);
+  transform: translateX(10px);
+  visibility: hidden;
+  opacity: 0;
+  -webkit-transition: 0.3s;
+  transition: 0.3s;
+}
+</style>
